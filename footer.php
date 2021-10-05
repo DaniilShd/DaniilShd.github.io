d7836db8c503500ac6998e4464d5ae2684cc6a6b
DaniilShd <91947634+DaniilShd@users.noreply.github.com>
2021-10-05 14:51:11 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!-- Footer start -->
+
+<footer class="footer">
+  <div class="wrapper">
+    <div class="row center-xs middle-xs">
+      <div class="col-sm-6 col-xs-4 box_logo">
+        <div class="">
+          <div class="footer_logo">
+            <img src="../static/img/svg/header-logo.svg" class="footer_logo-pic" alt="Агентство детских праздников">
+          </div>
+          <div class="logo_text">
+            <p>Агентство детских праздников<br>"Рич шоу" 2019-2021</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3 col-xs-8 end-xs">
+        <div class="soc_seti">
+          <a href="https://www.instagram.com/rich_show_msc"><i class="soc_seti_active soc_seti_instagram fab fa-instagram" style="cursor: pointer;"></i></a>&nbsp&nbsp
+          <a href="https://t.me/aleksandra_rich"><i class="soc_seti_active soc_seti_telegram fab fa-telegram" style="cursor: pointer;"></i></a>&nbsp&nbsp
+          <a href="https://api.whatsapp.com/send?phone=79176544174"><i class="soc_seti_active soc_seti_whatsapp fab fa-whatsapp" style="cursor: pointer;"></i></a>&nbsp&nbsp
+        </div>
+          <div class="phone_number_footer">
+              <a href="tel:+79176544174">+7 917-654-41-74</a>
+          </div>
+          <div class="phone_desc_footer">
+            <p>ежедневно с 9:00 до 23:00</p>
+          </div>
+      </div>
+      <div class="col-sm-3 col-xs-3 box-adres">
+            <p class="adres_footer_text"><i class="fas fa-map-marker-alt"></i> Адрес:<br>г. Москва, ул. Потаповская Роща, 6к1</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<!-- Footer end -->
+
+</body>
+<script src="../static/js/main.js"></script>
+<script src="../static/js/bootstrap.bundle.min.js"></script>
+
+
+</html>
